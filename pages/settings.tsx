052883bba16e31cfd68b6c709325114b665b0486
Jared Parkinson <jared@parkinson.im>
2020-02-16 10:19:27 -0700

    bugs

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -5,6 +5,13 @@ import { NextPage } from 'next';
 
 import { importFiles } from '../components/import-notes/import-notes/importFiles';
 import { Component } from 'react';
+import {
+  PouchyRx,
+  DBItem,
+} from '../components/import-notes/import-notes/PouchyRx';
+import { NoteSettings } from '../oith-lib/src/processors/NoteSettings';
+import { map } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
 function showProgress() {
   return (
     <progress className="progress is-small is-primary" max="100">
@@ -12,6 +19,40 @@ function showProgress() {
     </progress>
   );
 }
+
+function reloadSettings() {
+  let database = new PouchyRx(`v6-${window.location.hostname}-overlay-org`);
+
+  const save = (settingName: string, setting: DBItem<object | unknown>) => {
+    if (setting) {
+      localStorage.setItem(settingName, JSON.stringify(setting.doc));
+    }
+  };
+  const settings = database
+    .get<NoteSettings>(`eng-note-settings`)
+    .pipe(
+      map(noteSettings =>
+        save('eng-scriptures-overlay-noteSettings', noteSettings),
+      ),
+    );
+  const cats = database
+    .get(`eng-note-categories`)
+    .pipe(
+      map(noteSettings =>
+        save('eng-scriptures-overlay-noteCategories', noteSettings),
+      ),
+    );
+  const types = database
+    .get(`eng-note-types`)
+    .pipe(
+      map(noteSettings =>
+        save('eng-scriptures-overlay-noteTypes', noteSettings),
+      ),
+    );
+
+  return forkJoin(settings, cats, types);
+}
+
 export default class SettimgsPage extends Component {
   public state: { progress: boolean };
   render() {
@@ -25,13 +66,22 @@ export default class SettimgsPage extends Component {
           className={`button`}
           onClick={() => {
             this.setState({ progress: true });
-            importFiles('#fileUpload').subscribe(
-              () => {},
-              () => {},
-              () => {
-                this.setState({ progress: false });
-              },
-            );
+            importFiles('#fileUpload')
+              .pipe(map(() => {}))
+              .subscribe(
+                () => {},
+                () => {},
+                () => {
+                  this.setState({ progress: false });
+                  reloadSettings().subscribe(() => {
+                    window.history.back();
+
+                    setTimeout(() => {
+                      location.reload();
+                    }, 1000);
+                  });
+                },
+              );
           }}
         >
           Upload
